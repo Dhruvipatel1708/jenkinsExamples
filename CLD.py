10dd836d1b7b9b22728db0081bbd88c96abc325a
DhruviPatel <245122748042@mvsrec.edu.in>
2025-03-24 13:15:10 +0530

    Create CLD.py

---
+++ CLD.py
@@ -0,0 +1,13 @@
+import sys
+n1=int(sys.argv[1])
+n2=int(sys.argv[2])
+r1=n1+n2
+r1=n1-n2
+r1=n1*n2
+r1=n1/n2
+
+print("addition:",r1)
+print("sub:",r2)
+print("multiplication:",r3)
+print("division:",r4)
+~                     
